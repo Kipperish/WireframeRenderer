e3d8aa5a71d8e5a69c4fe97be37725e35c7b61cc
Kipperish <152880818+Kipperish@users.noreply.github.com>
2025-09-19 11:53:21 +0100

    Add files via upload

---
+++ Screen.py
@@ -0,0 +1,54 @@
+import pygame
+from Renderer import *
+from Shapes import *
+from pygame.locals import(
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT
+)
+
+screen = pygame.display.set_mode((1200, 600))
+pygame.display.set_caption("3D Render")
+clock = pygame.time.Clock()
+fps = 120
+
+origin = [(1200//2), (600//2)]
+shape = Cube()
+
+running = True
+while running:
+    clock.tick(fps)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+    screen.fill((0,0,0))
+
+    keys = pygame.key.get_pressed()
+    if keys[K_UP]:
+        focalLength += 1
+    if keys[K_DOWN]:
+        focalLength -= 1
+    if keys[K_RIGHT]:
+        yRotation += math.pi/180
+        shape.rotatedVertexMatrix = [rotateVertex(vertex, yRotationMatrix) for vertex in shape.vertexMatrix]
+    if keys[K_LEFT]:
+        yRotation -= math.pi/180
+        shape.rotatedVertexMatrix = [rotateVertex(vertex, yRotationMatrix) for vertex in shape.vertexMatrix]    
+
+    xRotationMatrix = [[1, 0, 0],
+                    [0, math.cos(xRotation), -math.sin(xRotation)],
+                    [0, math.sin(xRotation), math.cos(xRotation)]]
+
+    yRotationMatrix = [[math.cos(yRotation), 0, math.sin(yRotation)],
+                    [0, 1, 0],
+                    [-math.sin(yRotation), 0, math.cos(yRotation)]]
+
+    zRotationMatrix = [[math.cos(zRotation), -math.sin(zRotation), 0],
+                    [math.sin(zRotation), math.cos(zRotation), 0],
+                    [0, 0, 1]]
+
+    castModel(screen, (255,255,255), origin, shape.rotatedVertexMatrix, shape.edgeMatrix, focalLength)
+
+    pygame.display.flip()
